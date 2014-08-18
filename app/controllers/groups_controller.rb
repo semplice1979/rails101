762c84d1284090c1e5b21e29d2665bb15267621f
Sam <sam_hsu@yes123.com.tw>
2014-08-18 10:42:51 +0800

    Group CRUD OK

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,53 +6,50 @@ def index
 end
 def show
   @group = Group.find(params[:id])
-  @posts = @group.posts
+  #@posts = @group.posts
 end
 def new
   @group = Group.new
 end
 def create
-@group = Group.find(params[:id])
-
-if @group.save
-redirect_to groups_path
-else
-render :new
-end
+    @group = Group.new(group_params)
+  
+    if @group.save 
+      redirect_to groups_path, :notice => 'Add Group is done'
+    else
+      render :new
+    end
 end
 
 def edit
 @group = Group.find(params[:id])
+#@post = @group.posts.find(params[:id])
 end
 
 def update
 
-@group = Group.find(params[:id])
-if @group.update(group_params)
-redirect_to group_path(@group)
-else
-render :edit
-end
+    @group = Group.find(params[:id])
+  
+    if @group.update(group_params)
+      redirect_to groups_path, :notice => 'Edit Group is done'
+    else
+      render :edit
+    end
 end
 
 
 def destroy
-@group = Group.find(params[:id])
-@post = @group.posts.find(params[:id])
-@post.destroy
+   @group = Group.find(params[:id])
+    
+   @group.destroy
+   redirect_to groups_path, :alert => 'Delete Group'
+end
 
-redirect_to groups_path(@group)
-end
-def destroy
-@group = Group.find(params[:id])
-@group.destroy
-redirect_to groups_path
-end
 
 private
 
 
-def group_params
-params.require(:group).permit(:title, :description)
-end
+  def group_params
+      params.require(:group).permit(:title, :description)
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,51 +1,51 @@
 class PostsController < ApplicationController
   before_action :login_required, :only => [:new, :create, :edit,:update,:destroy]
 def new
-  @group = Group.find(params[:group_id])
-  @post = @group.posts.build
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.new
 end
 def create
-  #@group = Group.find(params[:group_id])
-  #@post = @group.posts.new(post_params)
-  @post = @group.posts.new(post_params)
-  @post.author = current_user
-
-  if @post.save
-  redirect_to group_path(@group)
-else
-  render :new
-end
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.new(post_params)
+  
+    if @post.save
+      redirect_to group_path(@group), :notice => 'Add POST is done¡I'
+    else
+      render :new
+    end
 end
 def edit
   #@group = Group.find(params[:group_id])
   #@post = @group.posts.find(params[:id])
-  @post = current_user.posts.find(params[:id])
+      @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
 end
 def update
-  @group = Group.find(params[:group_id])
-  @post = @group.posts.find(params[:id])
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
 
-if @post.update(post_params)
-  redirect_to group_path(@group)
-else
-  render :edit
-end
+    if @post.update(post_params)
+      redirect_to group_path(@group), :notice => 'POST edit is done'
+    else
+      render :edit
+    end
 end
 
 def destroy
   #@group = Group.find(params[:group_id])
   #@post = @group.posts.find(params[:id])
-  @post = current_user.posts.find(params[:id])
-  @post.destroy
-  @post.destroy
-  redirect_to group_path(@group), :alert => 'Deleted'
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
+
+    @post.destroy
+    redirect_to group_path(@group), :alert => 'POST DELETE'
 end
 
 
 private
 
-def post_params
-params.require(:post).permit(:content)
-end
+ def post_params
+    params.require(:post).permit(:content)
+  end
 
 end
