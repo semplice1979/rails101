b73afdc8b3901f4022a754adc9efac04c5ee067a
Sam <sam_hsu@yes123.com.tw>
2014-08-20 17:05:12 +0800

    rails 101 finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+puts 'Hello World!'
+puts "20 GROUPS with each 30 POST]"
+
+create_account = User.create([email: 'example@gmail.com', password: '12345678', password_confirmation: '12345678', name: 'TESTacount'])
+
+create_groups = for i in 1..20 do
+                  Group.create!([title: "Group no.#{i}", description: "example #{i} th GROUP", user_id: "1"])
+                  for k in 1..30 do
+                    Post.create!([group_id: "#{i}",content: "example #{k} th POST", user_id: "1"])
+                  end
+                end
